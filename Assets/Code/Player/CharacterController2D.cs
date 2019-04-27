00d8ebd44dfcaf73a9a73a37968b5325c74d231b
boaheck <breogan.hackett2@mail.dcu.ie>
2019-04-27 13:22:05 +0100

    missing files from previous commit

---
+++ Assets/Code/Player/CharacterController2D.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+
+	Rigidbody2D rigb;
+	BoxCollider2D col;
+	Vector2 pos, boxSize, boxOffset;
+	public LayerMask groundLayers;
+	public bool grounded, head, left, right;
+	public float timeSinceLastGrounded = 0.0f;
+
+	void Start () {
+		rigb = GetComponent<Rigidbody2D> ();
+		col = GetComponent<BoxCollider2D> ();
+		boxSize = (col.size + (Vector2.one * 2 * col.edgeRadius)) * 0.9f;
+		boxOffset = col.offset;
+		pos = transform.position;
+	}
+
+	public void setPos(Vector3 nPos){
+		transform.position = new Vector3 (nPos.x, nPos.y);
+		pos = nPos;
+	}
+
+	public void Move (Vector2 move) {
+		rigb.MovePosition (pos + move);
+	}
+
+	void Update () {
+		pos = new Vector2 (transform.position.x, transform.position.y);
+		grounded = Physics2D.OverlapBox (pos + boxOffset + (Vector2.down * 0.07f), boxSize,0,groundLayers.value);
+		head = Physics2D.OverlapBox (pos + boxOffset + (Vector2.up * 0.07f), boxSize,0,groundLayers.value);
+		left = Physics2D.OverlapBox (pos + boxOffset + (Vector2.left * 0.07f), boxSize,0,groundLayers.value);
+		right = Physics2D.OverlapBox (pos + boxOffset + (Vector2.right * 0.07f), boxSize,0,groundLayers.value);
+		if(grounded){
+			timeSinceLastGrounded = 0.0f;
+		}else{
+			timeSinceLastGrounded += Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Code/Player/PlatformerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerController : MonoBehaviour
+{
+    //Components
+    CharacterController2D character;
+
+    //status
+    float xVel = 0;
+    float yVel = 0;
+    bool moving = false;
+    bool running = false;
+    float currentSpeed = 0.0f;
+    float jumpPressed = 0.0f;
+    bool jumping = false;
+    bool jumpheld = false;
+
+    //Horizontal Movement
+    public float speed = 5.0f;
+    public float runSpeed = 10.0f;
+
+    //Vertical Movement
+    public float gravity = 9.0f;
+    public float fallMult = 2.0f;
+    public float jumpEndMult = 2.5f;
+    public float jumpSpeed = 10.0f;
+    public float landJumpTime = 0.05f;
+    public float coyoteTime = 0.03f;
+
+    void Start(){
+        //Setup
+        character = GetComponent<CharacterController2D>();
+        currentSpeed = speed;
+    }
+
+    void Update(){
+        //Handle input
+        running = Input.GetButton("Run");
+        if(running){
+            currentSpeed = runSpeed;
+        }else{
+            currentSpeed = speed;
+        }
+        if(Input.GetButtonDown("Jump")){
+            jumpPressed = landJumpTime;
+        }else if(jumpPressed > 0){
+            jumpPressed -= Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate() {
+        //handle this in fixed update to avoid differences in axis value during time between update and fixed update
+        moving = Mathf.Abs(Input.GetAxis("Horizontal")) > 0.001;
+
+        //Handle Movement   
+        if(moving){
+            xVel = Mathf.Sign(Input.GetAxis("Horizontal")) * currentSpeed;
+        } else {
+            xVel = 0;
+        }
+        character.Move(new Vector2(xVel,yVel) * Time.fixedDeltaTime);
+    }
+}
